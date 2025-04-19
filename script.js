a88f97a81c0c4476731f0947a324250f49f0360a
GugaToca <gustavotocalino08@outlook.com>
2025-04-19 11:21:26 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-let currentSlide = 0;
-const slides = document.querySelectorAll('.slide');
-
-function moveSlide(step) {
-    slides[currentSlide].classList.remove('active');
-    currentSlide = (currentSlide + step + slides.length) % slides.length;
-    slides[currentSlide].classList.add('active');
-}
